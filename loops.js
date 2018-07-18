29f485e15ba462d2ca9bad5a7e68a84e30709d69
Tiffany Choong <t.choong@yahoo.com>
2018-07-18 18:15:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -12,7 +12,7 @@ function whileLoop (n){
   return 'done';
 }
 
-function doWhileLoop(newArray){
+function doWhileLoop(array){
   function maybeTrue() {
   return Math.random() >= 0.5
 }
@@ -20,5 +20,5 @@ function doWhileLoop(newArray){
     array.pop();
   } while(maybeTrue());
   
-  return newArray;
+  return array;
 }
